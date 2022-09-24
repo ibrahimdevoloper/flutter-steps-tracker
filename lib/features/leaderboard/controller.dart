4a09b036ec4421beff08a64f09cb52675e4629ff
Ibrahimdeveloper <ibrahimdevoloper@gmail.com>
2022-09-24 23:46:33 +0300

    Leaderboard is linked

--- lib/features/leaderboard/controller.dart
+++ lib/features/leaderboard/controller.dart
@@ -1,5 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_steps_tracker/Models/user_data/user_data.dart';
+import 'package:flutter_steps_tracker/utilities/custom_snackbar.dart';
 import 'package:get/get.dart';
 
 class LeaderboardController extends GetxController {
+  late FirebaseFirestore _db;
+  var _isloading = false;
 
+  List<UserData> _users = [];
+
+  HomeController() {
+    _db = FirebaseFirestore.instance;
+    getData();
+  }
+
+  getData() async {
+    try {
+      _isloading = true;
+      update();
+      var userDocs = await _db
+          .collection("users")
+          .orderBy("step_count", descending: true)
+          .get();
+      for (var userDoc in userDocs.docs) {
+        _users.add(UserData.fromJson(userDoc.data()));
+      }
+
+      _isloading = false;
+      update();
+      showNotificationSnakebar("Sign in Success");
+    } catch (e) {
+      showErrorSnakebar("Error while getting data");
+      _isloading = false;
+      update();
+    }
+  }
+
+  List<UserData> get users => _users;
+
+  get isloading => _isloading;
 }
--- lib/features/leaderboard/view.dart
+++ lib/features/leaderboard/view.dart
@@ -12,62 +12,103 @@ class LeaderboardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Leaderboard"),
       ),
-      body: ListView.builder(itemBuilder: (context, i) {
-        return Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          child: ListTile(
-            leading: Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(24),
-                color: i%2==0?Theme.of(context).colorScheme.primary:Theme.of(context).colorScheme.secondary,
-              ),
-              child: Center(
-                child: Text(
-                  "1",
-                  style: Theme.of(context).textTheme.headline6!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: i%2==0?Theme.of(context).colorScheme.secondary:Theme.of(context).colorScheme.primary,
+      body: GetBuilder<LeaderboardController>(
+          init:controller,
+          builder: (controller) {
+        return ListView.builder(
+          itemCount: controller.users.length,
+            itemBuilder: (context, i) {
+            var user = controller.users[i];
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: ListTile(
+              leading: Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: i % 2 == 0 ? Theme
+                      .of(context)
+                      .colorScheme
+                      .primary : Theme
+                      .of(context)
+                      .colorScheme
+                      .secondary,
+                ),
+                child: Center(
+                  child: Text(
+                    i.toString(),
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline6!
+                        .copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: i % 2 == 0 ? Theme
+                          .of(context)
+                          .colorScheme
+                          .secondary : Theme
+                          .of(context)
+                          .colorScheme
+                          .primary,
+                    ),
                   ),
                 ),
               ),
-            ),
-            title: Text(
-              "Naiem",
-              style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.primary,
+              title: Text(
+                user.name,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Theme
+                      .of(context)
+                      .colorScheme
+                      .primary,
+                ),
+              ),
+              subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total steps: ${user.stepCount}",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .caption!
+                        .copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Theme
+                          .of(context)
+                          .colorScheme
+                          .primary
+                          .withOpacity(0.7),
+                    ),
+                  ), Text(
+                    "Redeemed points: ${user.redeemedPoints}",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .caption!
+                        .copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Theme
+                          .of(context)
+                          .colorScheme
+                          .primary
+                          .withOpacity(0.7),
+                    ),
+                  ),
+                ],
               ),
             ),
-            subtitle: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Total: 7000",
-                  style: Theme.of(context).textTheme.caption!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .primary
-                        .withOpacity(0.7),
-                  ),
-                ),Text(
-                  "Redeemed: 5000",
-                  style: Theme.of(context).textTheme.caption!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .primary
-                        .withOpacity(0.7),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
+          );
+        });
       }),
-    );  }
+    );
+  }
 }
