20b072da39b0978bb67ae31bd76a72c8d56d36b5
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2023-05-06 08:44:28 +0300

    changed directories and added privacy policies and terms of service.

---
+++ lib/features/html_display_page/view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+
+class HtmlDisplayPagePage extends StatelessWidget {
+  // final controller = Get.put(HtmlDisplayPageController());
+  final String title;
+  final String content;
+
+  const HtmlDisplayPagePage({
+    super.key,
+    required this.title,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(title),
+      ),
+      body: SingleChildScrollView(
+        child: Html(data: content),
+      ),
+    );
+  }
+}
