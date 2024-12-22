f1e772f38bbc4812ac06f85156842bfe3d462e1f
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-12-22 15:54:50 +0100

    removed extra code

--- lib/functions/date_formatter.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-String dateFormater(DateTime date, [String formate = 'yyyy-MM-dd']) {
-  final DateFormat formatter = DateFormat(formate);
-  String formatted = formatter.format(date);
-  // print(formatted);
-  return formatted;
-}
--- lib/utilities/project_constants.dart
+++
@@ -1,6 +0,0 @@
-class ProjectConstants{
-  static const userId = "userId";
-  static const username = "username";
-  static const isDarkMode = "isDarkMode";
-  static const isArabic = "isArabic";
-}
